2f68337808cab39bd69e42c07d310adf6a1f0a74
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-05-19 23:41:58 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 44 ms, Memory: 32412 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/20167
+
+import sys
+
+input = sys.stdin.readline
+N, K = map(int, input().split(" "))
+
+leafs = list(map(int, input().split(" ")))
+
+def dfs(i, prev, energy):
+  if i > N - 1:
+    return energy
+  
+  temp = dfs(i+1, prev, energy)
+  
+  prev += leafs[i]
+  if prev >= K:
+    energy += prev - K
+    prev = 0
+
+  return max(dfs(i+1, prev, energy), temp)
+
+
+print(dfs(0, 0, 0))
