176bbc0d86d146d899c835b15ad5cc00cbcaff4a
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-05-22 11:35:03 +0900

    [Gold V] Title: 암호 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -8,14 +8,18 @@ L, C = map(int, input().split())
 arr = input().split()
 arr.sort()
 
-vowel = ['a', 'e', 'i', 'o', 'u']
+vowel = set('aeiou')
 answer = []
 
 def re(prev, length, start, min_v, min_c):
-
+  global C
+  
   if length == 0:
     if min_v <= 0 and min_c <= 0:
       answer.append(prev)
+    return
+  
+  if length > C - start:
     return
   
   for i, a in enumerate(arr[start:]):
@@ -24,7 +28,7 @@ def re(prev, length, start, min_v, min_c):
     else:
       re(prev+a, length - 1, start + i + 1, min_v, min_c - 1)
 
-
 re("", L, 0, 1, 2)
+
 for a in answer:
-    print(a)
+  print(a)
