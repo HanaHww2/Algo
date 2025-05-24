26c09be5460a5fcaa8c736c607eace8ebc85a6b2
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-05-24 10:58:11 +0900

    [Silver I] Title: 나무 탈출, Time: 956 ms, Memory: 218436 KB -BaekjoonHub

---
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.py
@@ -0,0 +1,43 @@
+# https://www.acmicpc.net/problem/15900
+
+import sys
+from collections import deque 
+
+input = sys.stdin.readline
+
+N = int(input().strip())
+tree = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+  a, b = map(int, input().split())
+  
+  tree[a].append(b)
+  tree[b].append(a)
+
+depth = 0
+
+def make_depth(start, d):
+  global depth
+  visited = [0] * (N + 1)
+  q = deque([(start, d)])
+
+  while q:
+    s, d = q.popleft()
+    visited[s] = 1
+    is_leaf = True
+
+    for c in tree[s]:
+
+      if visited[c] != 1:
+        is_leaf = False
+        q.append((c, d + 1))
+          
+    if is_leaf:
+      depth += d
+      
+
+make_depth(1, 0)
+
+if depth % 2 == 0:
+  print('No')
+else: print('Yes')
