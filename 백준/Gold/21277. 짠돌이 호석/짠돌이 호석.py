cf6197852f763482fa62e24d9d7d2df32cb09dea
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-05-30 11:58:55 +0900

    [Gold III] Title: 짠돌이 호석, Time: 920 ms, Memory: 32412 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.py
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.py
@@ -5,19 +5,14 @@ import sys
 input = sys.stdin.readline
 
 N1, M1 = map(int, input().split())
-arr = [['0'] * 151 for _ in range(151)]
-min_r = min_c = float('inf')
-max_r = max_c = float('-inf')
+arr = [['0'] * 150 for _ in range(150)]
+
 extent = float('inf')
 
 for r in range(50, 50 + N1):
   str_list = input().strip()
   for c in range(50, 50 + M1):
     arr[r][c] = str_list[c-50]
-
-    if arr[r][c] == '0':
-      max_c = max(max_c, c)
-      max_r = max(max_r, r)
 
 N2, M2 = map(int, input().split())
 
@@ -28,23 +23,24 @@ for r in range(N2):
 def check():
   global extent
 
-  for r in range(50 - N2, max_r):
-    for c in range(50 - M2, max_c):
+  for r in range(50 - N2, 50 + N1 + 1):
+    for c in range(50 - M2, 50 + M1 + 1):
 
       is_possible = True
       
-      for nxt in range(len(arr2)):        
-        for mxt in range(len(arr2[0])):
+      for nxt in range(N2):        
+        for mxt in range(M2):
           val = arr[r+nxt][c+mxt]
           if val == '1' and arr2[nxt][mxt] != '0':
             is_possible = False
+            break
         
         if not is_possible:
           break
       
       if is_possible:
-        width = max(50 + N1, r + N2)- min(r, 50)
-        height = max(50 + M1, c + M2) - min(c, 50) 
+        height = max(50 + N1, r + N2) - min(r, 50)
+        width = max(50 + M1, c + M2) - min(c, 50) 
         extent = min(extent, width * height)
 
 # 4가지 케이스 (그대로, 90, 180, 270 회전)
@@ -57,6 +53,8 @@ for i in range(4):
   for row in zip(*arr2):
     temp.append(row[::-1])
   arr2 = temp
+  N2 = len(arr2)
+  M2 = len(arr2[0])
   
   check()
 
