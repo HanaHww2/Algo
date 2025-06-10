7b2ffb6e063ee0901cf306eb8c0905c7c0985622
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-06-10 12:11:45 +0900

    [level 2] Title: 택배상자, Time: 419.56 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -3,23 +3,13 @@ def solution(order):
     helper_c = []
     box = 1
     i = 0
-    print(i)
-    while i < len(order):
-        
-        if order[i] == box:
-            box += 1
-            i += 1
-            continue
-            
-        if helper_c and helper_c[-1] == order[i]:
-            helper_c.pop()
-            i += 1
-            continue
-        
-        if helper_c and helper_c[-1] > order[i]:
-            break
-        
+    while box < len(order) + 1:
         helper_c.append(box)
         box += 1
+          
+        while helper_c[-1] == order[i]:
+            helper_c.pop()
+            i += 1
+            if not helper_c: break
     
     return i
