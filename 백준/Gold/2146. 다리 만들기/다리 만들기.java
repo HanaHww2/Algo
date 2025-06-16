11de8ad0e6ba865fcce4e14b13f1faac1769eea8
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-06-16 11:10:40 +0900

    [Gold III] Title: 다리 만들기, Time: 216 ms, Memory: 81808 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -12,7 +12,8 @@ public class Main {
 
   static int N;
   static int[][] MAP;
-  static int cnt;
+
+  static int shortest;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,7 +35,8 @@ public class Main {
   }
 
   private static int getShortestBridge() {
-    int shortest = Integer.MAX_VALUE;
+
+    shortest = Integer.MAX_VALUE;
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         if (MAP[i][j] < 2) continue;
@@ -45,7 +47,7 @@ public class Main {
   }
 
   private static int bfs(int y, int x) {
-    boolean[][] visited = new boolean[N][N];
+    boolean[][] visitedSea = new boolean[N][N];
     Queue<int[]> q = new ArrayDeque<>(100);
     q.add(new int[] {y, x, 0});
     int from = MAP[y][x];
@@ -53,6 +55,7 @@ public class Main {
     while (!q.isEmpty()) {
       int[] node = q.poll();
 
+      if (node[2] - 1 >= shortest) break;
       // 다른 섬 도달
       if (MAP[node[0]][node[1]] != from && MAP[node[0]][node[1]] > 1) return node[2] - 1;
 
@@ -60,9 +63,8 @@ public class Main {
       for (int i = 0; i < dy.length; i++) {
         int ny = node[0] + dy[i], nx = node[1] + dx[i];
         if (ny < 0 || ny >= N || nx < 0 || nx >= N || MAP[ny][nx] == -1 || MAP[ny][nx] == from) continue;
-
-        if (visited[ny][nx]) continue;
-        visited[ny][nx] = true;
+        if (visitedSea[ny][nx]) continue;
+        if (MAP[ny][nx] == 0) visitedSea[ny][nx] = true; // 방문 바다 체크
 
         q.add(new int[] {ny, nx, node[2] + 1});
       }
@@ -77,7 +79,6 @@ public class Main {
         if (MAP[i][j] == 1) markMap(i, j, ++land);
       }
     }
-    cnt = land - 1;
   }
 
   private static void markMap(int y, int x, int land) {
