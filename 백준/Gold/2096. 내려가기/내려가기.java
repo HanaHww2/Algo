d22bdfd6c58e5971b1cbf713d1ec984938c09fba
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-06-21 11:49:28 +0900

    [Gold V] Title: 내려가기, Time: 344 ms, Memory: 42388 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] DP;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        DP = new int[3][2]; // min, max
+        StringTokenizer st;
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int lmin = Math.min(DP[0][0], DP[1][0]);
+            int rmin = Math.min(DP[1][0], DP[2][0]);
+            int lmax = Math.max(DP[0][1], DP[1][1]);
+            int rmax = Math.max(DP[1][1], DP[2][1]);
+
+            DP[0][0] = a + lmin;
+            DP[2][0] = c + rmin;
+            DP[1][0] = b + Math.min(lmin, rmin);
+
+            DP[0][1] = a + lmax;
+            DP[2][1] = c + rmax;
+            DP[1][1] = b + Math.max(lmax, rmax);
+        }
+
+        int max = Math.max(DP[2][1], Math.max(DP[0][1], DP[1][1]));
+        int min = Math.min(DP[2][0], Math.min(DP[0][0], DP[1][0]));
+        System.out.println(max + " " + min);
+    }
+}
