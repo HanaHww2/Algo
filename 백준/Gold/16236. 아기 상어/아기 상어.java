5f30df714bf23aa19f49616f12dd4466fc84a9c0
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-07-03 10:59:49 +0900

    [Gold III] Title: 아기 상어, Time: 112 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -27,58 +27,75 @@ public class Main {
                 if (WATER[i][j] == 9) SHARK = new int[]{i, j};
             }
         }
+        br.close();
 
-        System.out.println(search());
-    }
-
-    private static int search() {
-
+        WATER[SHARK[0]][SHARK[1]] = 0;
         int sec = 0;
         int eatingCnt = 0;
-        boolean[][] visited = new boolean[N][N];
-        PriorityQueue<int[]> pq = new PriorityQueue<>(getPreyComparator());
 
-        pq.offer(new int[]{0, SHARK[0], SHARK[1]});
-        visited[SHARK[0]][SHARK[1]] = true;
-        WATER[SHARK[0]][SHARK[1]] = 0;
+        while (true) {
+            Pos pos = search(SHARK[0], SHARK[1]);
+            if (pos == null) break;
 
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int dist = curr[0], y = curr[1], x = curr[2];
-
-            if (WATER[y][x] > 0 && WATER[y][x] < size) {
-                WATER[y][x] = 0;
-                eatingCnt++;
-                sec += dist;
-                dist = 0;
-                pq = new PriorityQueue<>(getPreyComparator());
-                visited = new boolean[N][N];
-            }
+            sec += pos.dist;
+            eatingCnt++;
             if (eatingCnt == size) {
                 size++;
                 eatingCnt = 0;
             };
 
+            SHARK[0] = pos.y;
+            SHARK[1] = pos.x;
+        }
+
+        System.out.println(sec);
+    }
+
+    private static Pos search(int sy, int sx) {
+
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        pq.offer(new Pos(sy, sx, 0));
+
+        boolean[][] visited = new boolean[N][N];
+        visited[sy][sx] = true;
+
+        while (!pq.isEmpty()) {
+            Pos curr = pq.poll();
+            int dist = curr.dist, y = curr.y, x = curr.x;
+
+            if (WATER[y][x] > 0 && WATER[y][x] < size) {
+                WATER[y][x] = 0;
+                return new Pos(y, x, dist);
+            }
+
             for (int i = 0; i < dy.length; i++) {
                 int ny = y + dy[i], nx = x + dx[i];
                 if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
                 if (WATER[ny][nx] > size || visited[ny][nx]) continue;
-                pq.add(new int[]{dist+1, ny, nx});
+                pq.add(new Pos(ny, nx, dist+1));
                 visited[ny][nx] = true;
             }
         }
-        return sec;
+        return null;
     }
 
-    private static Comparator<int[]> getPreyComparator() {
-        return (x, y) -> {
-            if (x[0] - y[0] == 0) {
-                if (x[1] - y[1] == 0) {
-                    return x[2] - y[2];
-                }
-                return x[1] - y[1];
+    static class Pos implements Comparable<Pos> {
+
+        int y, x, dist;
+
+        public Pos(int y, int x, int dist) {
+            this.y = y;
+            this.x = x;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            if (dist == o.dist) {
+                if (y == o.y) return x - o.x;
+                return y - o.y;
             }
-            return x[0] - y[0];
-        };
+            return dist - o.dist;
+        }
     }
 }
