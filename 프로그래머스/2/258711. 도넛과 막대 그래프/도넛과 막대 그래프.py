1d56df4093a2cb568e1dc7494d565d6d4cbec137
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-09-26 10:57:16 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1595.33 ms, Memory: 207 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -0,0 +1,48 @@
+import sys
+from collections import defaultdict, Counter
+
+sys.setrecursionlimit(10**6)
+
+def solution(edges):
+    answer = []
+    
+    graph = defaultdict(list)
+    candidate = set()
+    visited = set()
+    has_in = set()
+    
+    for e in edges:
+        graph[e[0]].append(e[1])
+        
+        has_in.add(e[1])
+        
+        if e[0] not in has_in:
+            candidate.add(e[0])
+        if e[1] in candidate:
+            candidate.remove(e[1])
+        
+    if not candidate:
+        return [0, 0, 0, 0]  
+    
+    addition = max(candidate, key=lambda i: len(graph[i]))
+    answer.append(addition)
+        
+    def search(node:int, start:int):
+        cnt = 0
+        for nxt in graph[node]:
+            if len(graph[node]) > 1:
+                return 2
+            if nxt == start:
+                return 1
+            cnt = max(cnt, search(nxt, start))
+        return cnt
+    
+        
+    couter = Counter()
+    for nxt in graph[addition]:
+        couter[search(nxt, nxt)] += 1
+        
+    for i in [1, 0, 2]:
+        answer.append(couter[i])
+    
+    return answer
