02749c8cde346aafce49f69904eb93ba3ffe37bc
HanaH <62924471+HanaHww2@users.noreply.github.com>
2025-10-01 12:06:21 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1070.65 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,46 +1,30 @@
 def solution(cap, n, deliveries, pickups):
+    pd = dd = n
     
+    while deliveries and not deliveries[-1]:
+        deliveries.pop()
+        dd -= 1
+    while pickups and not pickups[-1]:
+        pickups.pop()
+        pd -= 1
     answer = 0
-    dist = n
-    pd = dist - 1
-    dd = dist - 1    
-    while dist > 0:
-        answer += dist * 2
-        
-        pc = cap
-        dc = cap
-        flagP = True
-        flagD = True
-        while flagD or flagP:
-            
-            if flagP and pd > -1:
-                if pickups[pd] > pc:
-                    pickups[pd] -= pc
-                    pc = 0
-                    flagP = False
-                else:
-                    pc -= pickups[pd]
-                    pickups[pd] = 0
-                    pd -= 1
-                    flagP = True
+    while deliveries or pickups:
+        answer += max(dd, pd) * 2
+
+        dc = pc = cap
+        while deliveries:
+            if deliveries[-1] <= dc:
+                dc -= deliveries.pop()
+                dd -= 1
             else:
-                flagP = False
-            if flagD and dd > -1:
-                if deliveries[dd] > dc:
-                    deliveries[dd] -= dc
-                    dc = 0
-                    flagD = False
-                else:
-                    dc -= deliveries[dd]
-                    deliveries[dd] = 0
-                    dd -= 1
-                    flagD = True
+                deliveries[-1] -= dc
+                break
+        while pickups:
+            if pickups[-1] <= pc:
+                pc -= pickups.pop()
+                pd -= 1
             else:
-                flagD = False    
-            
-            if not flagP and not flagD:
-                dist = max(pd, dd) + 1
-                if answer == n*2 and dist == 0 and dc == cap and pc == cap:
-                    return 0
-        
+                pickups[-1] -= pc
+                break
+
     return answer
